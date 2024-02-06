d6c0c5bfbf348cacce6ab4b9fda5e427ad8f8630
Katie Bridson <kbridson@memphis.edu>
2024-02-05 18:43:18 -0600

    Replaced hardcoded text string with filepath input and read file content

--- check_palindrome.py
+++ check_palindrome.py
@@ -3,7 +3,16 @@ def check_palindrome(text):
         print(f"{text} is a palindrome")
     else:
         print(f"{text} is not a palindrome")
-
-
-text = input("Enter text to check for palindrome: ")
-check_palindrome(text)
+choice = input("Enter 't' for text or 'f' for file: ")
+if choice == 't':
+    text = input("Enter text to check for palindrome: ")
+    check_palindrome(text)
+elif choice == 'f':
+    try:
+        filepath = input("Enter filepath for palindrome check: ")
+        text = open(filepath, "r").read()
+        check_palindrome(text)
+    except OSError:
+        print(f"Unable to process file at {filepath}")
+else:
+    print("Invalid choice")
